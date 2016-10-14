e229422d34511cf20d3ae2f7fa2ffd3b80371a3c
Nick Rigano <nrigano@Executor.local>
2016-10-14 16:00:58 -0400

    In progress?

--- HellaCollectionCells/HellaViewController.swift
+++ HellaCollectionCells/HellaViewController.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+//
+//INSTRUCTIONS
+//Drag a collection view (not a controller!) on to the existing view controller named, HellaViewController.
+//Populate your collection view with 1000 cells.
+//If the index of the cell is included in the Fibonacci Sequence, set the cell background color to purple. Otherwise set it to yellow.
+//When a cell is tapped, it should take the user to a new view controller named HellaDetailViewController. The index number should be dislayed as a string of a label's text property. Additionally, the view's background color (not the label's) should be set to the corresponding color.
+
 import UIKit
 
 class HellaViewController: UIViewController {
@@ -20,3 +27,27 @@ class HellaViewController: UIViewController {
     }
 }
 
+
+
+
+
+
+
+// structs classes and enums
+//func fibonacci {
+//    switch case cellNumber =
+//}
+//
+//func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//    return 1000
+//}
+//
+//func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hellaCell", for: indexPath)
+//    
+//    
+//    
+//    
+//    cell.backgroundColor = UIColor.fibonacci()
+//    return cell
+//}
